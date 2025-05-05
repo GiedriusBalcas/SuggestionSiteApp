4b272fc6870cf352e38904bf7ee6af0ac1aaee77
Giedrius Balčas <giedrius.balcas@gmail.com>
2025-05-05 15:22:34 +0300

    implemented mongo data access.

---
+++ SuggestionAppLibrary/DataAccess/MongoStatusData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SuggestionAppLibrary.DataAccess;
+public class MongoStatusData : IStatusData
+{
+   private const string _cacheName = "StatusData";
+   private readonly IMemoryCache _cache;
+   private readonly IMongoCollection<StatusModel> _statuses;
+
+   public MongoStatusData(IDbConnection db, IMemoryCache cache)
+   {
+      _cache = cache;
+      _statuses = db.StatusCollection;
+   }
+
+   public async Task<List<StatusModel>> GetAllStatuses()
+   {
+      var output = _cache.Get<List<StatusModel>>(_cacheName);
+
+      if (output is null || output.Count == 0)
+      {
+         var results = await _statuses.FindAsync(_ => true);
+         output = results.ToList();
+
+         _cache.Set(_cacheName, output, TimeSpan.FromDays(1));
+      }
+
+      return output;
+   }
+
+   public Task CreateStatus(StatusModel status)
+   {
+      return _statuses.InsertOneAsync(status);
+   }
+}
